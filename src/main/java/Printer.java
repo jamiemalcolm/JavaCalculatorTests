4afa054747f59b1efb91020e4c0579d0d1a3264e
jamiemalcolm <jmalcolm281@gmail.com>
2021-02-08 17:01:25 +0000

    all extentions passed

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,18 +1,25 @@
 public class Printer {
 
     private int pagesLeft;
+    private int tonerLevel;
 
-    public Printer(int pagesLeft){
+    public Printer(int pagesLeft, int tonerLevel){
         this.pagesLeft = pagesLeft;
+        this.tonerLevel = tonerLevel;
     }
 
     public int getPagesLeft() {
         return this.pagesLeft;
     }
 
+    public int getTonerLevel() {
+        return this.tonerLevel;
+    }
+
     public void print(int pages, int copies) {
-        while (copies > 0 && this.pagesLeft >= pages){
+        while (copies > 0 && this.pagesLeft >= pages && this.tonerLevel > 0){
             this.pagesLeft = this.pagesLeft - pages;
+            this.tonerLevel = this.tonerLevel - pages;
             copies = copies -1;
         }
     }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -8,35 +8,61 @@ public class PrinterTest {
     Printer printer;
 
     @Before
-    public void before(){
-        printer = new Printer(10);
+    public void before() {
+        printer = new Printer(10, 100);
     }
+
     @Test
-    public void hasNumberOfSheetsLeft(){
+    public void hasNumberOfSheetsLeft() {
         assertEquals(10, printer.getPagesLeft());
     }
 
     @Test
-    public void printThreeCopiesOfTwoPages(){
-        printer.print(2,3);
+    public void printThreeCopiesOfTwoPages() {
+        printer.print(2, 3);
         assertEquals(4, printer.getPagesLeft());
     }
 
     @Test
-    public void printThreeCopiesOfFourPages(){
-        printer.print(4,3);
+    public void printThreeCopiesOfFourPages() {
+        printer.print(4, 3);
         assertEquals(2, printer.getPagesLeft());
     }
 
     @Test
-    public void printTwoCopiesOfFivePages(){
-        printer.print(5,2);
+    public void printTwoCopiesOfFivePages() {
+        printer.print(5, 2);
         assertEquals(0, printer.getPagesLeft());
     }
 
     @Test
-    public void printOneCopiesOf11Pages(){
-        printer.print(11,1);
+    public void printOneCopiesOf11Pages() {
+        printer.print(11, 1);
         assertEquals(10, printer.getPagesLeft());
+    }
+
+    @Test
+    public void printerHasTonerLevel() {
+        assertEquals(100, printer.getTonerLevel());
+    }
+
+    @Test
+    public void printerTonerLevelReducedByOne() {
+        printer.print(1, 1);
+        assertEquals(99, printer.getTonerLevel());
+    }
+
+    @Test
+    public void printerTonerLevelLowerThanPagesLeft_CheckPagesLeft() {
+        Printer lowTonerPrinter = new Printer(5, 4);
+        lowTonerPrinter.print(1, 5);
+        assertEquals(1, lowTonerPrinter.getPagesLeft());
+    }
+
+    @Test
+    public void printerTonerLevelLowerThanPagesLeft_CheckTonerLevel() {
+        Printer lowTonerPrinter = new Printer(5, 4);
+        lowTonerPrinter.print(1, 5);
+        assertEquals(0, lowTonerPrinter.getTonerLevel());
     }
 }
