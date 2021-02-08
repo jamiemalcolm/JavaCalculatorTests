11d3ead677fc6e6c16eb50ef8940046d9f773d6c
jamiemalcolm <jmalcolm281@gmail.com>
2021-02-08 16:47:12 +0000

    test for enough paper passed, second test written

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -9,4 +9,11 @@ public class Printer {
     public int getPagesLeft() {
         return this.pagesLeft;
     }
+
+    public void print(int pages, int copies) {
+        while (copies > 0 && this.pagesLeft >= pages){
+            this.pagesLeft = this.pagesLeft - pages;
+            copies = copies -1;
+        }
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,6 +1,7 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class PrinterTest {
@@ -15,5 +16,21 @@ public class PrinterTest {
         assertEquals(10, printer.getPagesLeft());
     }
 
+    @Test
+    public void printThreeCopiesOfTwoPages(){
+        printer.print(2,3);
+        assertEquals(4, printer.getPagesLeft());
+    }
 
+    @Test
+    public void printThreeCopiesOfFourPages(){
+        printer.print(4,3);
+        assertEquals(2, printer.getPagesLeft());
+    }
+    
+    @Test
+    public void printTwoCopiesOfFivePages(){
+        printer.print(5,2);
+        assertEquals(0, printer.getPagesLeft());
+    }
 }
