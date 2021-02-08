8e40a8d2237ebe3edeae903ecc49d8522dc77050
jamiemalcolm <jmalcolm281@gmail.com>
2021-02-08 15:43:17 +0000

    calculator tests passed

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -10,4 +10,22 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
         assertEquals(4, calculator.add(2, 2));
     }
+
+    @Test
+    public void testSubtract(){
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.subtract(10, 5));
+    }
+
+    @Test
+    public void testMultiply(){
+        Calculator calculator = new Calculator();
+        assertEquals(10, calculator.multiply(2, 5));
+    }
+
+    @Test
+    public void testDivide(){
+        Calculator calculator = new Calculator();
+        assertEquals(10, calculator.divide(20, 2), 0.001);
+    }
 }
